4096fbec97f8ef75de8ac7bc20e661fb220ebc2f
Dmitrii <kalla.mallanik@gmail.com>
2026-07-21 17:46:04 +0300

    supress httpx logs

--- main.py
+++ main.py
@@ -24,6 +24,9 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+# приглушаем шумные HTTP-логи
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 load_dotenv()
 
